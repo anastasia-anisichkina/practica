816bf7a02962ec4de92dd83bf0a9182b970bccf2
anastasia-anisichkina <nastya_pavlova19@list.ru>
2021-12-27 22:55:59 +0300

    высчитываем четные элементы массива

---
+++ zadanie1/Program.cs
@@ -0,0 +1,12 @@
+﻿Random random = new Random();
+const int N = 10;
+int[] vector = new int[N];
+ 
+Console.WriteLine("Исходный массив");
+ 
+for (int i = 0; i < N; i++)
+    vector[i] = random.Next(1, 9);
+ 
+Console.WriteLine(string.Join(" ", vector.Select(x =>x.ToString())));
+ 
+int[] evenArr = vector.Where(x => x%2==0).ToArray();
