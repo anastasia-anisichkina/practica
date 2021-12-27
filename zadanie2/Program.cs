f80bca4ffb738ea48f5cad82675da9d90843a07f
anastasia-anisichkina <nastya_pavlova19@list.ru>
2021-12-27 23:21:36 +0300

    заполнилит массив

--- zadanie2/Program.cs
+++ zadanie2/Program.cs
@@ -3,3 +3,8 @@ Random random = new Random();
 const int N = 15;
 int[] vector = new int[N];
 // zapolnili massiv
+Console.WriteLine("Исходный массив");
+ 
+for (int i = 0; i < N; i++)
+    vector[i] = random.Next(1, 15);
+    
