ac342e4d6ab6bf9465ef964e4d58b01498791e7b
anastasia-anisichkina <nastya_pavlova19@list.ru>
2021-12-27 23:24:30 +0300

    провелили числа больше 8

--- zadanie2/Program.cs
+++ zadanie2/Program.cs
@@ -7,4 +7,7 @@ Console.WriteLine("Исходный массив");
  
 for (int i = 0; i < N; i++)
     vector[i] = random.Next(1, 15);
-    
+// проверяем числа больше 8 
+
+Console.WriteLine(string.Join(" ", vector.Select(x =>x.ToString())));
+int[] evenArr = vector.Where(x => x > 8).ToArray();
